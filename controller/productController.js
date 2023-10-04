43b5d38f05dab1d598acbbcd42334b07e55305fa
Ikbarsf <ikbarsaif@gmail.com>
2023-10-04 21:54:44 +0700

    first commit

---
+++ controller/productController.js
@@ -0,0 +1,49 @@
+const { Product } = require("../models");
+
+const getAllProduct = async (req, res) => {
+  const { name, price, stock } = req.body;
+  try {
+    const newProduct = await Product.create({
+      name,
+      price,
+      stock,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        newProduct,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
+
+const createProduct = async (req, res) => {
+  const { name, price, stock } = req.body;
+  try {
+    const newProduct = await Product.create({
+      name,
+      price,
+      stock,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        newProduct,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
+
+module.exports = {
+  createProduct,
+};
---
+++ routes/productRouter.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+const Product = require("../controller/productController");
+
+router.post("/", Product.createProduct);
+
+module.exports = router;
